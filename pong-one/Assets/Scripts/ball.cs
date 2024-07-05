77ebbec0f89a0b3ecd7194e0101738bc89eda4d3
unknown <emarkle99@gmail.com>
2024-07-05 17:52:24 -0500

    ball name

--- pong-one/Assets/Scripts/ball.cs
+++ pong-one/Assets/Scripts/ball.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ball : MonoBehaviour
+public class Ball : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
